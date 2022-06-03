fe7ef8c399c672af2569cd1e0bf1b560685b68d2
DELLAPIAZZA FLORES Magalí Valentina <mdellapiazza@correo.um.edu.uy>
2022-06-03 16:28:12 -0300

    Agregado tests de Stack y LinkedList.

---
+++ test/LinkedListTest.java
@@ -0,0 +1,99 @@
+import org.junit.jupiter.api.Test;
+import uy.edu.um.prog2.adt.LinkedList.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @Test
+    void add() {
+
+
+        LinkedList<Integer> sut = new LinkedList<>(); // test de add para LinkedList primera posición
+        sut.add(1);
+        assertEquals(new Integer(1), sut.get(0));
+
+        sut.add(null);
+        assertEquals(2, sut.getSize());
+
+//        falta agregar exception de lista vacía (que si yo estoy tratando de obtener la posición de una lista vacía me tire una excepción).
+//        LinkedList<Integer> lista2 = new LinkedList<>();
+//        assertNull(lista2.get(0), new LinkedList<>());
+//        cuando este vacía te tire una excepción
+//        cuando te tire una posicion invalida te tira una excepción.
+    }
+
+    @Test
+    void remove() {
+        //        AGREGAR EXCEPTION SI HAGO UN GET DE UNA LISTA VACÍA DEBE TIRAR EXCEPCIÓN
+
+        LinkedList<Integer> sut = new LinkedList<>();
+        sut.add(1);
+        sut.add(2);
+        sut.remove(1);
+        assertEquals(new Integer(1), sut.get(0));
+
+    }
+
+    @Test
+    void get() {
+        LinkedList<Integer> sut = new LinkedList<>();
+        sut.add(1);
+        assertEquals(1, sut.get(0));
+//       AGREGAR EXCEPTION LISTA VACÍA
+
+    }
+
+    @Test
+    void removeValue() {
+        LinkedList<String> sut = new LinkedList<>();
+        sut.add("Hola");
+        sut.add("Mundo");
+        sut.removeValue("Mundo");
+        assertEquals("Hola", sut.get(0));
+//      AGREGAR EXCEPTION LISTA VACÍA
+    }
+
+    @Test
+    void inList() {
+        LinkedList<String> sut = new LinkedList<>();
+        sut.add("A");
+        sut.add("B");
+        sut.add("C");
+        assertTrue(sut.inList("B"));
+
+//        AGREGAR EXCEPTION LISTA VACÍA
+    }
+
+    @Test
+    void addFirst() {
+        LinkedList<Integer> sut = new LinkedList<>();
+        sut.add(12);
+        sut.add(16);
+        sut.add(8);
+        sut.addFirst(20);
+        assertEquals(20, sut.get(0));
+    }
+
+    @Test
+    void display() {
+
+    }
+
+    @Test
+    void getHead() {
+
+    }
+
+    @Test
+    void setHead() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void setSize() {
+    }
+}
---
+++ test/StackTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTest {
+
+    @Test
+    void pop() {
+    }
+
+    @Test
+    void top() {
+    }
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void makeEmpty() {
+    }
+}
