eedde3d92b657f26b759d082ed3f37cb433ec7bc
DELLAPIAZZA FLORES Magalí Valentina <mdellapiazza@correo.um.edu.uy>
2022-06-07 15:40:55 -0300

    Agregado Utils base para main.

---
+++ src/entities/Utils.java
@@ -0,0 +1,20 @@
+package entities;
+import java.util.Scanner;
+
+public class Utils {
+
+    public static void DisplayMenu() {
+        String input;
+        Scanner myObj = new Scanner(System.in);  // Crea un objeto Scanner.
+        System.out.println("Bienvenido. Ingrese el número correspondiente a lo que desea realizar: ");
+        System.out.println("1- Cargar datos");
+        System.out.println("2- Realizar consultas");
+//        input = myObj.nextInt(); //para el input, para leer como entero.
+//
+
+
+/*    while(){
+
+    }*/
+    }
+}
